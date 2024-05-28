e629dfd3980318b7ffc9b328f220d21038563eab
shu-sha <ksemenova02@mail.ru>
2024-05-28 19:55:48 +0700

    добавление простого автотеста

---
+++ lesson_2_task_12.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try: 
+    link = "http://suninjuly.github.io/explicit_wait2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+    WebDriverWait(browser, 15).until(EC.text_to_be_present_in_element((By.ID, "price"), '100'))
+    button = browser.find_element(By.ID, "book")
+    button.click()
+    pic = browser.find_element(By.CSS_SELECTOR, "[id='input_value']")
+    browser.execute_script("return arguments[0].scrollIntoView(true);", pic)
+    x = pic.text
+    y = calc(x)
+    
+    input1 = browser.find_element(By.ID, "answer")
+    input1.send_keys(y)
+    
+    # Отправляем заполненную форму
+    button2 = browser.find_element(By.CSS_SELECTOR, "[id='solve']")
+    button2.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
